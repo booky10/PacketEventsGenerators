adf7a53ced3b5bb8ed1eec9f817f92016d6e330e
booky10 <boooky10@gmail.com>
2024-06-16 05:33:14 +0200

    Implement new block state generator
    Old, uncomitted stuff

--- src/main/java/dev/booky/generation/generators/BlockMappingsGenerator.java
+++ src/main/java/dev/booky/generation/generators/BlockMappingsGenerator.java
@@ -1,63 +1,52 @@
 package dev.booky.generation.generators;
 // Created by booky10 in MinecraftSource (19:02 05.09.23)
 
-import net.minecraft.SharedConstants;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonPrimitive;
+import dev.booky.generation.util.GenerationUtil;
 import net.minecraft.core.registries.BuiltInRegistries;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.block.state.properties.Property;
+import org.apache.commons.lang3.math.NumberUtils;
 
-import java.io.BufferedWriter;
 import java.io.IOException;
-import java.io.Writer;
-import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.List;
 import java.util.Map;
 
 public final class BlockMappingsGenerator implements IGenerator {
 
-    @SuppressWarnings({"rawtypes", "unchecked"}) // I don't care
-    private static void stringifyStateValues(Writer writer, Map<Property<?>, Comparable<?>> values) throws IOException{
-        if (values.isEmpty()) {
-            return; // no values present, nothing to stringify
-        }
-
-        writer.write('[');
-        boolean first = true;
-        for (Map.Entry<Property<?>, Comparable<?>> entry : values.entrySet()) {
-            if (first) {
-                first = false;
-            } else {
-                writer.write(',');
-            }
-
-            Property<?> property = entry.getKey();
-            writer.write(property.getName());
-            writer.write('=');
-
-            String valueName = ((Property) property).getName(entry.getValue());
-            writer.write(valueName);
-        }
-        writer.write(']');
-    }
-
     @Override
     public void generate(Path outDir, String genName) throws IOException {
-        Path outPath = outDir.resolve(genName + ".txt");
-        try (BufferedWriter writer = Files.newBufferedWriter(outPath)) {
-            writer.write(SharedConstants.getCurrentVersion().getId());
+        JsonArray array = new JsonArray();
+        for (Block block : BuiltInRegistries.BLOCK) {
+            List<BlockState> states = block.getStateDefinition().getPossibleStates();
+            int defaultIndex = states.indexOf(block.defaultBlockState());
 
-            for (BlockState state : Block.BLOCK_STATE_REGISTRY) {
-                writer.newLine();
-                if (state.getBlock().defaultBlockState() == state) {
-                    writer.append('*'); // default state
+            JsonArray entries = new JsonArray();
+            for (BlockState state : states) {
+                JsonObject object = new JsonObject();
+                for (Map.Entry<Property<?>, Comparable<?>> entry : state.getValues().entrySet()) {
+                    String valueStr = ((Property) entry.getKey()).getName(entry.getValue());
+                    JsonPrimitive value = NumberUtils.isDigits(valueStr)
+                            ? new JsonPrimitive(Integer.parseInt(valueStr))
+                            : "true".equals(valueStr) ? new JsonPrimitive(true)
+                            : "false".equals(valueStr) ? new JsonPrimitive(false)
+                            : new JsonPrimitive(valueStr);
+                    object.add(entry.getKey().getName(), value);
                 }
-
-                ResourceLocation blockKey = BuiltInRegistries.BLOCK.getKey(state.getBlock());
-                writer.append(blockKey.getPath());
-                stringifyStateValues(writer, state.getValues());
+                entries.add(object);
             }
+
+            JsonObject object = new JsonObject();
+            object.addProperty("type", BuiltInRegistries.BLOCK.getKey(block).getPath());
+            object.add("entries", entries);
+            object.addProperty("def", defaultIndex);
+            array.add(object);
         }
+
+        GenerationUtil.saveJsonElement(array, outDir.resolve(genName + ".json"));
     }
 }
